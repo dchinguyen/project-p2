2525ff21de7227ad8bfcbbb7c02f5333096d1d6c
dchinguyen <106204167@student.swin.edu.au>
2025-11-13 21:41:45 +0700

    about 1

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php include("header.inc"); include("nav.inc"); ?>
+<main>
+<h2>About Our Project Team</h2>
+<table border="1" cellpadding="8">
+<tr><th>Member</th><th>Student ID</th><th>Contribution</th></tr>
+<tr><td>Nguyen Duc Hai </td><td>SWH</td><td>Backend & Database (manage.php, MySQL)</td></tr>
+<tr><td>Tran Thi B</td><td>987654321</td><td>Frontend & Accessibility (HTML/CSS)</td></tr>
+
+</table>
+<p><a href="manage.php">Go to Management Page →</a></p>
+</main>
+<?php include("footer.inc"); ?>
