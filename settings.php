90bc256ff5c109bc42cdc3c4ab856e640d39fcdc
dchinguyen <106204167@student.swin.edu.au>
2025-11-17 02:01:57 +0700

    fix

--- settings.php
+++ settings.php
@@ -1,11 +1,12 @@
 <?php
-$DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = '';
-$DB_NAME = 'project2_db';
+$host = 'localhost';
+$user = 'root';
+$pwd  = '';
+$sql_db = 'project2_db';
 
-$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS);
-if (!$conn) { die('DB connect failed: '.mysqli_connect_error()); }
+$conn = mysqli_connect($host, $user, $pwd);
+if (!$conn) die('Database connection failed.');
 
-mysqli_query($conn, "CREATE DATABASE IF NOT EXISTS `$DB_NAME` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
-mysqli_select_db($conn, $DB_NAME);
+mysqli_query($conn, "CREATE DATABASE IF NOT EXISTS `$sql_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
+mysqli_select_db($conn, $sql_db);
+?>
